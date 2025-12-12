6fff5e15dc5d5e3de12d4c11ef81f97e5a962bf0
amenir88 <ameni.rommene@esprit.tn>
2025-12-12 15:50:21 +0100

    Workshop 12:Authgard

--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -5,10 +5,14 @@ import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 
 import { FormsModule } from '@angular/forms';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
   declarations: [
+    SigninComponent,
+    SignupComponent,
     UsersComponent,
   ],
   imports: [
